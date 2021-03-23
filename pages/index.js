4e502777a4b9fb9dae4602fa4e8a25fda71cf937
yumaasato <asson0730@icloud.com>
2021-03-23 14:20:34 +0900

    first-post.jsを追加した

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link'
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="container">
       <Head>
@@ -10,7 +11,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Read{' '}
+        <Link href="/posts/first-post">
+          <a>this page!</a>
+        </Link>
         </h1>
 
         <p className="description">
@@ -207,3 +211,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
