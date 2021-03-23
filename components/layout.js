5708f9e2eb7af84e92151cd5a26ab221496858a2
yumaasato <asson0730@icloud.com>
2021-03-23 16:19:56 +0900

    globalCSSを導入した

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,6 @@
-function Layout({ children }) {
-  return <div>{children}</div>
+import styles from './layout.module.css';
+
+const Layout = ({ children }) => {
+  return <div className={styles.container}>{children}</div>
 }
 export default Layout;
