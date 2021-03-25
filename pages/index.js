54db06a882028f8d99ea86cc161b954ea9537fad
yumaasato <asson0730@icloud.com>
2021-03-25 13:02:28 +0900

    ブログのデータを出力できるようにした

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,44 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-const Home = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    //propsというキーに対してオブジェクト型で返す
+    props: {
+      allPostsData
+    }
+  }
+}
+
+const Home = ({ allPostsData }) => {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>九州大学法学部</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://twitter.com/satopin2020">our Twitter</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
